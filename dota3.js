0c75c29e983c8fc3950f85aeefa2d5dc421fe1aa
McLovin-v <ivanbaki02@gmail.com>
2024-12-14 21:52:16 +0200

    dota 3 you
    test

--- dota3.js
+++ dota3.js
@@ -1,42 +1,84 @@
 console.log("invok");
-const sidebar = document.getElementById('sidebar');
-const openBtn = document.getElementById('open-btn');
 const closeBtn = document.getElementById('close-btn');
 const spellsBtn = document.getElementById('spells_list');
 const spells = document.getElementById('spells');    
 const aboutBox = document.getElementById('about-box');
 const aboutBoxBtn = document.getElementById('about_list');
-
-openBtn.addEventListener('click', () =>{
-    console.log('open');
-    sidebar.style.left = '0';
+const imagDiv = document.getElementById('invok');
+const nowSpell = document.getElementById('game-text');
+const p = document.getElementById ('p');
+const o = document.getElementById ('o');
+const sidebar = document.getElementById ('sidebar');
+const open_menu = document.getElementById ('open-btn');
+var key_q ='KeyQ';
+var key_w ='KeyW';
+var key_e ='KeyE';
+const bind = document.getElementById ('bind');
+const bindText = document.getElementById ('Bind');
+bind.addEventListener("keydown", function(event) {
+console.log (event.code);
+key_q =event.code
 });
+bindText.addEventListener ("click" , ()=>{
+    bindText
+        spells.style.visibility = 'hidden';
+        aboutBox.style.visibility = 'hidden';
+        
+    bind.style.visibility= 'visible';
+    
+    }
+    
+);
 
-closeBtn.addEventListener('click', () => {
-    console.log('close');
-    sidebar.style.left = '-250px';
-    spells.style.visibility = 'hidden';
+
+
+var sidebar1 = false;
+open_menu.addEventListener("click", ()=>{
+    if (sidebar1==true){
+        sidebar1=false;
+        sidebar.style.left = '-250px';
+        spells.style.visibility = 'hidden';
+        aboutBox.style.visibility = 'hidden';
+    }else{
+        sidebar1=true;
+        sidebar.style.left = '0';
+    }
+});
+spellsBtn.addEventListener('click', () => {
     aboutBox.style.visibility = 'hidden';
-});
-
-spellsBtn.addEventListener('click', ()=>{
-    aboutBox.style.visibility ='hidden';
     spells.style.visibility = 'visible';
 });
 
-let spell = ['q', 'w', 'e'];
- 
 const magic_spells = [
-    ['q', 'w', 'e'],
-    ['q', 'q', 'q'],
-    ['w', 'w', 'w'],
-    ['e', 'e', 'e'],
-    ['q', 'e', 'w']
-]
+    [key_q, key_w, key_e],
+    [key_q, key_q, key_q],
+    [key_w, key_w, key_w],
+    [key_e, key_e, key_e],
+    [key_q, key_q, key_e],
+    [key_w, key_w, key_e],
+    [key_w, key_e, key_e],
+    [key_q, key_e, key_e],
+    [key_q, key_q, key_w],
+    [key_q, key_w, key_w],
+];
+var spellImage = {
+    'KeyQKeyQKeyQ': `invoker-cold-snap.webp`,
+    'KeyQKeyWKeyE': `invoker-deafening-blast-5390-050025ad4c4a95aec8fbcb2c46cb850fa2bbba51f2b5200024791492fef94a45.jpg`,
+    'KeyQKeyQKeyE': `invoker-ice-wall-5389-5b21c01affced71613f6bf8ce5236fdde928b1d6aa2ed912329efdf6ee5581d8.jpg`,
+    'KeyWKeyWKeyW': `invoker-e-m-p-5383-88e380494cbc94fecd1a5554656591537640a9bef95a47b8e632496137d69608.jpg`,
+    'KeyEKeyEKeyE': `png-klev-club-cvcl-p-sferi-invokera-png-20.png`,
+    'KeyWKeyEKeyE': `invoker-chaos-meteor-5385-b93d7e77232fa51479d0d1c1453d0c61bb187c055c7b05ac541ba7b809bc30d5.jpg`,
+    'KeyWKeyWKeyE': `alacriti.jpg`,
+    'KeyQKeyEKeyE': `invoker-forge-spirit-5387-85c20a082bc67b1892330af760affdb2ce1ccb53a1b2a90ce02f43e2028cdef9.jpg`,
+    'KeyQKeyQKeyW': `image1.png`,
+    'KeyQKeyWKeyW': `image2.png`
+};
+
+
+
 
 let currentSpellIndex = 0;
 let userInput = [];
-let currentSpell = [];
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -51,27 +93,64 @@ let shuffledSpells = shuffleArray([...magic_spells]);
 function displayCurrentSpell() {
     const spellsDiv = document.getElementById('game-text');
     if (spellsDiv) {
-        spellsDiv.innerHTML = `
-        <div>now spelling: ${shuffledSpells[currentSpellIndex].join('')}</div>
-        <div>your text: ${userInput.join('')}</div>
-        `;
+        const spell = shuffledSpells[currentSpellIndex];
+        nowSpell.innerHTML = `<img src = "imeg/${spellImage[spell.join('')]}" width = 250px >`
+        console.log(`<img src = "imeg/${spellImage[spell.join('')]}">`);
+
     }
 }
 
+
+function spellsImage(userInput) {
+    imagDiv.innerHTML = ``;
+    userInput.forEach(key => {
+        let image = ``;
+        if (key === key_q) {
+            image = `<img src="imeg/Quas_icon.webp" alt="Quas">`;
+        } else if (key === key_w) {
+            image = `<img src="imeg/Wex_icon.webp" alt="Wex">`;
+        } else if (key === key_e) {
+            image = `<img src="imeg/Exort_icon.webp" alt="Exort">`;
+        }
+        if (image) {
+            imagDiv.innerHTML += image;
+        }
+    });
+}
+
+
+function displayCurrentImage(){
+    const currentSpellCombo = shuffledSpells[currentSpellIndex]
+    let imageString = '';
+    
+    for(let i = 0;i<=2;i++){
+        imageString +=currentSpellCombo[i];
+        
+
+
+    }
+
+    let image = `<img src = "imeg/${spellImage[imageString]}"  width = 250px  >`
+     
+     nowSpell.innerHTML = image;
+}
+
 document.addEventListener('keydown', function(event) { 
-    if (['q', 'w', 'e'].includes(event.key)) {
+    if ([key_q, key_w, key_e].includes(event.code)) {
         userInput.push(event.key);
-        if (userInput.length > 3) {
+
+        if (userInput.length > 12) {
             userInput.shift();
         }
         displayCurrentSpell();
+        spellsImage(userInput);
+        displayCurrentImage();
     }
+    else {console.log ('3')}
     if (event.key.toLowerCase() === 'r') {
         checkSpell();
     }
 });
-
-
 function areSpellsEqual(spell1, spell2) {
     if (spell1.length !== spell2.length) return false;
 
@@ -96,23 +175,39 @@ function checkSpell() {
     if (userInput.length === 3) {
         const currentSpellCombo = shuffledSpells[currentSpellIndex];
         const isCorrect = areSpellsEqual(userInput, currentSpellCombo);
-    
+        
         if (isCorrect) {
             currentSpellIndex++;
+            imegString = currentSpellCombo.join("");
+            o.src= p.src;
+
+            imegString = currentSpellCombo.join("");
+
+            p.src ="imeg/"+ spellImage[imegString];
+            console.log(`image spell: ${currentSpellCombo}/ src: ${spellImage[imegString]}/ p:${p}`);
+
+          
+
+
+                console.log (spellImage [currentSpellCombo]);
+
             if (currentSpellIndex >= shuffledSpells.length) {
-                console.log("Win");
                 currentSpellIndex = 0;
                 shuffledSpells = shuffleArray([...magic_spells]);
             }
         }
+
         
-        userInput = [];
+     
         displayCurrentSpell();
     }
+  
 }
 
-aboutBoxBtn.addEventListener('click', ()=>{
+aboutBoxBtn.addEventListener('click', () => {
     aboutBox.style.visibility = 'visible';
     spells.style.visibility = 'hidden';
 });
+
 displayCurrentSpell();
+
